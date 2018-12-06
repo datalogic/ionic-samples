67e4dad6c2e662c38d4279a0cb0767e890cddf44
Sachin Shankar <sachin.shankar@datalogic.com>
2018-12-06 10:18:57 -0800

    Initial commit

---
+++ SetTriggers/src/pages/home/home.ts
@@ -0,0 +1,69 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+declare let keyboardManager : any;
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+  triggers  = [];	
+
+  constructor(public navCtrl: NavController) {
+  	keyboardManager.getAllAvailableTriggers(
+  		(data) => {
+
+  			//var obj = data.triggers;
+  			this.triggers =  JSON.parse(JSON.parse(data).triggers);
+
+  			//alert(arr.length);
+  			// var arr = data.triggers;
+  			// for(var i=0; i<arr.length; i++){
+  			// 	this.triggers.push(arr[i]);
+  			// }
+  			//alert(this.triggers);
+  		},
+  		(err) => {
+  			alert(err);
+  		});
+  }
+
+  getAllAvailableTriggers(){
+
+  	keyboardManager.getAllAvailableTriggers(
+  		(data) => {
+  			alert(JSON.parse(data).triggers);
+  		},
+  		(err) => {
+  			alert(err);
+  		});
+  }
+
+  setAllAvailableTriggers(enable){
+  	keyboardManager.setAllAvailableTriggers(
+  		enable,
+  		(data) => {
+  			alert(data);
+  			//update the local data structure
+  			for(var i=0; i<this.triggers.length; i++){
+  				this.triggers[i].enabled = enable;
+  			}
+  		},
+  		(err) => {
+  			alert(err);
+  		});
+  }
+
+  setTriggers(){
+  	keyboardManager.setTriggers(
+  		this.triggers,
+  		(data) => {
+  			alert(data);
+  		},
+  		(err) => {
+  			alert(err);
+  		});
+  }
+
+}
